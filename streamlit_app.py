fae92c8cb6da18ed57ef9a4e4a4e7ffbd05afe67
KathiravanRajamani <64543829+KathiravanRajamani@users.noreply.github.com>
2025-10-23 23:24:34 -0400

    Update streamlit_app.py
    Convert dataframe to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -27,8 +28,13 @@ session = cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+# Convert my_dataframe to pandas dataframe so that we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 
 # Add Multiselect
 
